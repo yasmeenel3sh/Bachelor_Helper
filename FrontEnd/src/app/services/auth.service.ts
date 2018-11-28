15d489b8555054550b4e87dd2abb495d6f261c14
yasmeen el3sh <yasmeen.abdelmohsen@student.guc.edu.eg>
2018-11-28 17:22:53 +0200

    auth service frontend

---
+++ FrontEnd/src/app/services/auth.service.ts
@@ -0,0 +1,65 @@
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+  export class AuthService {
+    constructor(private http: HttpClient, private router: Router) {
+    }
+
+   public   signup(data){
+    let signUp={
+     name:data.name,
+     email:data.email.trim(),
+     password:data.password.trim(),
+     confirmPassword:data.confirmPassword.trim(),
+     major:data.major,
+     bachCountry:"",
+     bachUni:"",
+     info:""
+    }
+    return this.http.post('auth/signup', signUp);
+  }
+
+   public  login(email,password) {
+        var reqHeader = new HttpHeaders({'Content-Type':'application/json'});
+        return this.http.post('auth/login', {email:email,password:password},{headers:reqHeader});
+      
+      }
+    
+
+    public logout() {
+        localStorage.removeItem('userToken');
+        //to do navigate to home 
+        this.router.navigateByUrl("/")
+      }
+
+
+    public getToken(): string {
+        return localStorage.getItem('userToken');
+      }
+
+
+
+     //check if user logged in
+    public isAuthenticated() {
+        // get the token
+        const token = this.getToken();
+        // return a boolean reflecting 
+        // whether or not the token is expired
+        return token !== undefined && token !== null;
+      }
+
+
+      //helps in restricted view pages
+     public redirectIfAuthenticated(route: string) {
+        if (this.isAuthenticated()) {
+          this.router.navigateByUrl(route);
+        }
+      }
+    
+  }
