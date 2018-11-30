949ed1d737252007ffb8875b5ce700a84545acb8
OlfatMostafa <OlfatMostafa@users.noreply.github.com>
2018-11-30 17:23:48 +0200

    fixed search error

--- FrontEnd/src/app/services/auth.service.ts
+++ FrontEnd/src/app/services/auth.service.ts
@@ -1,86 +1,86 @@
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {environment } from '../../environments/environment'
+import { environment } from '../../environments/environment'
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root'
+})
 
-  export class AuthService {
-    constructor(private http: HttpClient, private router: Router) {
-      const helper = new JwtHelperService();
+export class AuthService {
+  constructor(private http: HttpClient, private router: Router) {
+    const helper = new JwtHelperService();
 
-    }
-
-   public   signup(data){
-     var major=(""+data.major).toLocaleLowerCase().trim().split(" ").join("");
-    let signUp={
-     name:data.name,
-     email:data.email.trim(),
-     password:data.password.trim(),
-     confirmPassword:data.confirmPassword.trim(),
-     major:major,
-     bachCountry:"",
-     bachUni:"",
-     bachYear:"",
-     info:""
-    }
-    return this.http.post(environment.domain+'auth/register', signUp);
   }
 
-   public  login(email,password) {
-        var reqHeader = new HttpHeaders({'Content-Type':'application/json'});
-        return this.http.post(environment.domain+'auth/login', {email:email,password:password},{headers:reqHeader});
-      
-      }
-    
+  public signup(data) {
+    var major = ("" + data.major).toLocaleLowerCase().trim().split(" ").join("");
+    let signUp = {
+      name: data.name,
+      email: data.email.trim(),
+      password: data.password.trim(),
+      confirmPassword: data.confirmPassword.trim(),
+      major: major,
+      bachCountry: "",
+      bachUni: "",
+      bachYear: "",
+      info: ""
+    }
+    return this.http.post(environment.domain + 'auth/register', signUp);
+  }
 
-    public logout() {
-        localStorage.removeItem('userToken');
-        //to do navigate to home 
-        this.router.navigateByUrl('/home');
-      }
+  public login(email, password) {
+    var reqHeader = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(environment.domain + 'auth/login', { email: email, password: password }, { headers: reqHeader });
 
-
-    public getToken(): string {
-        return JSON.stringify(localStorage.getItem('userToken'));
-      }
-
-
-
-     //check if user logged in
-    public isAuthenticated() {
-        // get the token
-        const token = localStorage.getItem('userToken');
-       
-        // return a boolean reflecting 
-        // whether or not the token is expired
-        return token != undefined && token != null;
-      }
+  }
 
 
-      //helps in restricted view pages
-     public redirectIfAuthenticated(route: string) {
-        if (this.isAuthenticated()) {
-          this.router.navigateByUrl(route);
-        }
-      }
-      public getUserFromToken(token: any) {
-        let payload;
-        if (token) {
-          payload = token.split('.')[1];
-          payload = window.atob(payload);
-          return JSON.parse(payload).user;
-        } else {
-          return null;
-        }
-      }
-      public getCurrentUser() {
-        return this.getUserFromToken(this.getToken());
-      }
-    public getUserName(){
-      return this.getCurrentUser().name;
+  public logout() {
+    localStorage.removeItem('userToken');
+    //to do navigate to home 
+    this.router.navigateByUrl('/home');
+  }
+
+
+  public getToken(): string {
+    return JSON.stringify(localStorage.getItem('userToken'));
+  }
+
+
+
+  //check if user logged in
+  public isAuthenticated() {
+    // get the token
+    const token = localStorage.getItem('userToken');
+
+    // return a boolean reflecting 
+    // whether or not the token is expired
+    return token != undefined && token != null;
+  }
+
+
+  //helps in restricted view pages
+  public redirectIfAuthenticated(route: string) {
+    if (this.isAuthenticated()) {
+      this.router.navigateByUrl(route);
     }
   }
+  public getUserFromToken(token: any) {
+    let payload;
+    if (token) {
+      payload = token.split('.')[1];
+      payload = window.atob(payload);
+      return JSON.parse(payload).user;
+    } else {
+      return null;
+    }
+  }
+  public getCurrentUser() {
+    return this.getUserFromToken(this.getToken());
+  }
+  public getUserName() {
+    return this.getCurrentUser().name;
+  }
+}
--- FrontEnd/src/app/user-profile/user-profile.component.ts
+++ FrontEnd/src/app/user-profile/user-profile.component.ts
@@ -1,79 +1,96 @@
-import { Component, OnInit} from '@angular/core';
-import {FormBuilder,FormGroup, Validators,ReactiveFormsModule, FormsModule} from "@angular/forms";
-import {UserDTO} from "./data/userDTO";
-import {AuthService} from '../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { UserDTO } from './data/userDTO';
+import { AuthService } from '../services/auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css'],
-  providers: [FormBuilder,ReactiveFormsModule,FormsModule,UserDTO,],
- 
+  providers: [FormBuilder, ReactiveFormsModule, FormsModule, UserDTO],
+
 })
 
+
+
 export class UserProfileComponent implements OnInit {
-   currentUser;
+  // we get the user by id in here
+   updatedUser: UserDTO;
+  currentUser;
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': localStorage.getItem('userToken')
+    })
+  };
+
+
 
   formData: FormGroup = this.formBuilder.group({
-    name: ['usr',[Validators.required]],
-    country: ['egy',[Validators.required]],
+    name: ['usr', [Validators.required]],
+    bachCountry: ['egy', [Validators.required]],
+    bachYear: ['2016'],
+    bachUni: ['guc'],
     major: ['met'],
-    year: ['2016'],
-    university: ['guc'],
-    info:['  ']
-    
+    info: ['  ']
+
   });
 
- editProfile:boolean=false;
-  constructor(private auth:AuthService,private formBuilder: FormBuilder,private reactiveFormModule: ReactiveFormsModule) { 
-          //static data replaced by the call to the method .
-            
+  editProfile = false;
+
+  constructor(private http: HttpClient, private auth: AuthService,
+     private formBuilder: FormBuilder, private reactiveFormModule: ReactiveFormsModule) {
+     this.updatedUser = new UserDTO();
 
   }
 
   ngOnInit() {
-    //get the userDTo here from the data base
-   this.currentUser=this.auth.getCurrentUser();
-   console.log(this.currentUser);
-    // this.currentUser=new UserDTO();
-    // this.currentUser.email="ahmed.alaa@gmail.com";
-    // this.currentUser.country="egypt";
-    // this.currentUser.major="cs";
-    // this.currentUser.university="guc";
-    // this.currentUser.name="ahmed";
-    // this.currentUser.year="1998";
-    // this.currentUser.info="My bach Abroad was one of the best experiences that i had in life .I encourage every studet o go to the same country i was and reside in the same place i was its just wonderfull experience full of enjoyment and learning ,i will give here info about the place i resided and the phone number of the man to rent from and give full description of transportation food money you will job opurtunities there and much more ";
-    // this.currentUser.info=2;
-    
-  }
-  initialize ():void{
+    // get the userDTo here from the data base
+    this.currentUser = this.auth.getCurrentUser();
 
-    this.formData.controls.year.setValue(this.currentUser.year); 
+    console.log(this.currentUser);
+
+  }
+  initialize(): void {
+    // we initialiae the form with tha data of the user who wants to edit
     this.formData.controls.name.setValue(this.currentUser.name);
-    this.formData.controls.country.setValue(this.currentUser.country);
+    this.formData.controls.bachCountry.setValue(this.currentUser.bachCountry);
+    this.formData.controls.bachYear.setValue(this.currentUser.bachYear);
+    this.formData.controls.bachUni.setValue(this.currentUser.bachUni);
     this.formData.controls.major.setValue(this.currentUser.major);
-    this.formData.controls.university.setValue(this.currentUser.university);
-    this.formData.controls.bachDetails.setValue(this.currentUser.info);
+    this.formData.controls.info.setValue(this.currentUser.info);
   }
-  //copy the user data in the form .
-   enableEdit(): void{
-    this.initialize();
-     console.log(this.formData);
-       this.editProfile=true;
-      
-  }
-//submit the user data edited to the db with the id 
-//take the data from the form inside the userDTO to send todb
-onSubmit():void{
- this.currentUser.year= this.formData.controls.year.value;
- this.currentUser.major= this.formData.controls.major.value;
- this.currentUser.university= this.formData.controls.university.value;
- this.currentUser.country= this.formData.controls.country.value;
- this.currentUser.info= this.formData.controls.bachDetails.value;
- this.currentUser.name= this.formData.controls.name.value;
-//here is the service that updates a user
-  this.editProfile=false; 
-console.log(this.currentUser);
 
-}
+  // copy the user data in the form .
+  enableEdit(): void {
+    this.editProfile = true;
+    this.initialize();
+
+
+  }
+  // submit the user data edited to the db with the id
+  // take the data from the form inside the userDTO to send todb
+  onSubmit(): void {
+    this.updatedUser.name = this.formData.controls.name.value;
+    this.updatedUser.bachYear = this.formData.controls.bachyear.value;
+    this.updatedUser.bachUni = this.formData.controls.bachUni.value;
+    this.updatedUser.bachCountry = this.formData.controls.bachCountry.value;
+    this.updatedUser.major = this.formData.controls.major.value;
+    this.updatedUser.info = this.formData.controls.info.value;
+    // here is the service that updates a user
+
+
+    this.http.patch(environment.domain + 'user/update', this.currentUser, this.httpOptions)
+      .subscribe((data: any) => {
+        this.editProfile = false;
+        console.log(data);
+      }, (err: any) => {
+        console.log(err);
+      });
+
+  }
+
 }
