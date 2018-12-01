dde6b5f32e2461fb408a7ce27b0708f9e8eeea98
nourhanelfransawy <nourhan.frensh@gmail.com>
2018-12-01 20:30:46 +0200

    fix the route of the login

--- FrontEnd/src/app/login/login.component.ts
+++ FrontEnd/src/app/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
   constructor(private auth:AuthService, private router:Router) { }
@@ -13,14 +15,12 @@ export class LoginComponent implements OnInit {
   private errorMsg;
   ngOnInit() {
   }
-  login(form){
 
+  login(form){
     this.auth.login(form.value.email.trim(),form.value.password).subscribe((data : any)=>{
       localStorage.setItem('userToken',data.data);
-      this.router.navigate(['/home']);
-     window.location.reload();
-
-     
+      this.router.navigate(['home']);
+     //window.location.reload();
     },
   (err :any)=>{
     this.errorMsg=err.error.msg;
