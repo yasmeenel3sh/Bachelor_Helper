74334371d57ba0f6b3eebfc9870160255673b114
yasmeen el3sh <yasmeen.abdelmohsen@student.guc.edu.eg>
2018-12-03 00:21:15 +0200

    added comments

--- BackEnd/api/config/index.js
+++ BackEnd/api/config/index.js
@@ -8,6 +8,6 @@ module.exports = {
             ? ''
             : process.env.NODE_ENV === 'test'
             ? 'mongodb://localhost:27017/Bachtest'
-            : 'mongodb://database/Bachelor_Helper',
+            : 'mongodb://database/Bachelor_Helper', //mongodb://localhost:27017/Bachelor_Helper
     EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 };
