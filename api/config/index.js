e47a53d14ab49d40634ad2480a2cbe66a848f2ed
olfatmostafa <olfat.aaf@student.guc.edu.eg>
2018-12-03 15:24:48 +0200

    modifying structure for deployment

--- api/config/index.js
+++ api/config/index.js
@@ -1,12 +1,13 @@
 // Global App Configuration
 module.exports = {
     //BACKEND_URI: 'http://localhost:3000/',
+    ENV: 'dev',
     FRONTEND_URI: process.env.FRONTEND_URI || 'http://localhost:4200/',
     SECRET: '32876qihsdh76@&#!742(*#HG&#28702y&##@^!()(&^#))jhscbd',
     MONGO_URI:
-       // process.env.NODE_ENV === 'production'
-            '',
-        //    : 'mongodb://localhost:27017/Bachelor_Helper', //'mongodb://database/Bachelor_Helper',//
+        process.env.NODE_ENV === 'production'
+            ? 'mongodb://<x>:<123456a>@ds161793.mlab.com:61793/bachelor-helper'
+            : 'mongodb://localhost:27017/Bachelor_Helper', //'mongodb://database/Bachelor_Helper',//
     EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 };
 
